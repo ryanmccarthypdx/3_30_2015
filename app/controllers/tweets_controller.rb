169f0c4d857afdfd8a6cd74899a3517594c7f47b
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-30 12:00:57 -0700

    Add full rails functionality, need to start replacing stuff with ajax

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,18 +9,41 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def show
+    @tweet= Tweet.find(params[:id])
+  end
+
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
     if @tweet.save
       redirect_to tweets_path
     else
-      render :new
+      redirect_to :back
     end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path
+    else
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
   end
 
   private
   def tweet_params
-    params.require(:tweet).permit(:text, :user_id)
+      params.require(:tweet).permit(:text, :user_id)
   end
 end
