4e28b5e765acf8ee7c6f2f5a67f8f78510a0cc5b
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-30 09:58:52 -0700

    Add maximum text length for tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  validates_length_of :text, :maximum => 140, :allow_blank => false
+
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Tweet do
 
   it { should belong_to :user }
+  it { should validate_length_of :text }
 
 end
